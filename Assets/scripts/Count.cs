fdfd74aa54a42ba3c1008cce2280f4b47090a0f5
KA-gamecreate <burakki.efi.22@icloud.com>
2020-06-07 16:27:25 +0900

    全部終わった

---
+++ Assets/scripts/Count.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Count : MonoBehaviour
+{
+
+    public Text text;
+    public int number;
+    public Text damage;
+    public int total;
+    public AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = 0;
+        sound = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    public void CountUp() {
+
+        number = number + 1;
+        text.text = number.ToString();
+    }
+    public void Attack()
+    {
+        total += number;
+        damage.text = total.ToString();
+        sound.PlayOneShot(sound.clip);
+    }
+}
